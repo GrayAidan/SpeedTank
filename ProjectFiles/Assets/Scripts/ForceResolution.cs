055cec8cb054c8fb945e282bceb7658669e5d32f
GrayAidan <114709817+GrayAidan@users.noreply.github.com>
2022-11-01 12:04:32 -0400

    File Addition
    This addition only contains the basic project files. The scene contains the split screen and thats it.

---
+++ ProjectFiles/Assets/Scripts/ForceResolution.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceResolution : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(3840, 1200, FullScreenMode.Windowed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
